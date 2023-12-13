55654d654d34cc3b7663569f8b98c1707ab43ee5
Maritedo <62166314+Maritedo@users.noreply.github.com>
2023-12-13 09:24:12 +0800

    ?

--- LET ME DOWNload it!.user.js
+++ LET ME DOWNload it!.user.js
@@ -13,37 +13,82 @@
 
     const useDebug = true;
 
+    const a = window.console.log;
+    window.console.log = () => { };
+    window.console.info = () => { };
+    window.console.warn = () => { };
+    window.console.error = () => { };
     function log(...objs) {
         if (!useDebug) return;
         const error = new Error();
         const callerLine = error.stack.split('\n')[2].trim().split(':');
         const len = callerLine.length - 2;
-        console.log(`[LMDI:${callerLine[len]}]`, ...objs);
+        a(`[LMDI:${callerLine[len]}]`, ...objs);
     }
 
     let name = "", href = "";
-    let iframe, title;
+    let iframe, title, exits = false;
 
     function download() {
+        if (!exits) {
+            return alert("未找到资源");
+        }
         log(`尝试下载${name}: ${href}`)
         downloadName(href, name);
+    }
+
+    let def = "#ffad1698";
+    const state = {
+        empty: () => {
+            exits = false;
+            label.style.backgroundColor = "#3339";
+            setHref("");
+        },
+        okay: () => {
+            exits = true;
+            label.style.backgroundColor = def;
+            setHref(href);
+        }
     }
 
     function update() {
         /** @type {string} url*/
         iframe = document.querySelector('iframe');
         title = document.querySelector("h5.title.flex_panel.hor");
-        let url = iframe.src;
-        href = url.slice(url.indexOf('furl') + 5/** 偏移值为"furl="的长度+1 */);
-        name = title.textContent;
+        if (!iframe.src) {
+            state.empty();
+        } else {
+            state.okay();
+            let url = iframe.src;
+            href = url.slice(url.indexOf('furl') + 5/** 偏移值为"furl="的长度+1 */);
+            name = title.textContent;
+        }
+    }
+
+    let setHref;
+
+    let observer;
+    function listenOn(ele) {
+        !observer && (observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                if (mutation.type === 'childList') {
+                    log("URL changed");
+                    update();
+                }
+            });
+        }))
+        observer.observe(ele, {
+            childList: true
+        });
     }
 
     window.onload = () => {
+        window.console.clear();
         log("Injected");
         listenerElements(
             "#root", () => {
                 log("插入按钮");
-                createButton();
+                setHref = createButton();
             })
             .then(
                 "#xy_app_content"
@@ -53,17 +98,29 @@
             )
             .final(
                 _ => {
-                    log("页面载入完成")
-                    var observer = new MutationObserver(function (mutations) {
+                    log("页面载入完成");
+                    // window.addEventListener('popstate', function (event) {
+                    //     log("URL Change");
+                    //     update();
+                    // });
+                    const taframe = document.querySelector("div.ta-frame")
+                    const _observer = new MutationObserver(function (mutations) {
                         mutations.forEach(function (mutation) {
                             if (mutation.type === 'childList') {
-                                update();
+                                log("Page changed");
+                                let content;
+                                if ((content = taframe.querySelector("div.group-resource-header"))) {
+                                    log("Found content dom");
+                                    listenOn(content);
+                                } else {
+                                    a(taframe)
+                                }
                             }
                         });
                     });
-                    observer.observe(document.querySelector("div.group-resource-content"), {
+                    _observer.observe(taframe, {
                         childList: true
-                    });
+                    })
                     update();
                 }
             )
@@ -145,6 +202,8 @@
         })
     }
 
+    let label, anchor;
+
     function createButton() {
         const _label = document.createElement("label");
         Object.assign(_label.style, {
@@ -152,30 +211,39 @@
             textDecoration: "none",
             userSelect: "none",
             "-webkit-user-select": "none",
-            boxShadow: "0 - 6px 8px - 8px rgba(0, 0, 0, 0.1), 0 - 9px 6px 0 rgba(0, 0, 0, 0.1), 0 - 12px 48px 16px rgba(0, 0, 0, 0.2)",
+            boxShadow: "0 -3px 8px -8px rgba(0, 0, 0, 0.1), 0 5px 6px 0 rgba(0, 0, 0, 0.1), 0 -8px 18px 5px rgba(0, 0, 0, 0.05)",
             position: "fixed",
             bottom: "48px",
             right: "48px",
             borderRadius: "24px",
             width: "48px",
             height: "48px",
-            backgroundColor: "#6665",
+            backgroundColor: def,
             backdropFilter: "blur(3px)",
-            "-webkit-backdrop-filter": "blur(3px)"
+            "-webkit-backdrop-filter": "blur(3px)",
+            transiton: "all .2s ease"
 
         });
         const _achor = document.createElement("a");
         Object.assign(_achor.style, {
             display: "inline-block",
-            color: "#fff",
+            color: "#000",
             lineHeight: "48px",
             width: "100%",
             textAlign: "center"
         });
         _achor.textContent = "下载";
         _achor.addEventListener("click", e => download());
+        _achor.onclick = e => {
+            e.preventDefault();
+        }
 
         _label.appendChild(_achor);
         document.body.appendChild(_label);
+
+        label = _label;
+        anchor = _achor;
+
+        return (url) => _achor.href = url;
     }
 })(window, document);
